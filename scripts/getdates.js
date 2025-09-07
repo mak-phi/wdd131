816e7eb0724f3f78ee17c9a6d70688b1ed03a3e9
mak-phi <mkmakiti@gmail.com>
2025-09-07 21:39:03 +0300

    w01 assignment : homepage

---
+++ scripts/getdates.js
@@ -0,0 +1,13 @@
+// Select the DOM element for output
+const currentYear = document.getElementById("currentyear");
+
+// Create the date object, extract the year value, and set the content to the DOM element
+const today = new Date();
+
+currentYear.textContent = today.getFullYear();
+
+// Set the last modified data-time
+const lastModified = document.lastModified;
+document.getElementById("lastModified").innerHTML = `Last Modification: ${lastModified}`;
+
+console.log("This script is running");
