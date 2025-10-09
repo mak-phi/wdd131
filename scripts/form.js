f3cab480f008f41fe49d4a7f5925dd8b4e599467
mak-phi <mkmakiti@gmail.com>
2025-10-09 18:25:34 +0300

    w05 assignment: product review form

---
+++ scripts/form.js
@@ -0,0 +1,48 @@
+const productSelectionList = document.querySelector("#product");
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
+
+function populateProducts() {
+    products.forEach((product) => {
+        let option = document.createElement("option");
+        option.setAttribute("value", product.name);
+        option.textContent = capitalizeWords(product.name);
+
+        productSelectionList.append(option);
+    });
+}
+
+function capitalizeWords(words) {
+    let splitWords = words.split(" ");
+    let formattedWords = splitWords.map((word) => {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    return formattedWords.join(" ");
+}
+
+populateProducts();
